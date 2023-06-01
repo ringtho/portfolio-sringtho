b3f158898ba436198e66ea6d162a3da1ed8009cf
Smith Ringtho <sringtho@gmail.com>
2023-06-01 21:27:27 +0300

    Added the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {
+  About,
+  Header,
+  Footer,
+  Skills,
+  Testimonial,
+  Work
+} from './container/index'
+import { Navbar } from './components/index'
+import './App.scss'
 
 function App () {
   return (
-    <div className="App">
-      Smith Ringtho
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.scss'
+import { images } from '../../constants'
+
+function Navbar () {
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map(item => (
+          <li key={`link-${item}`} className='app__flex p-text'>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
