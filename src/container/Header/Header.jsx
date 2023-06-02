c60196e73ebffc1e8d11b6657e9cb99f3f1e324b
Smith Ringtho <sringtho@gmail.com>
2023-06-02 06:23:39 +0300

    Added hero picture and content

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import './Header.scss'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
 
 function Header () {
   return (
-    <div>
-      Header
+    <div className='app_header app_flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+       <div className='app__header-badge'>
+        <div className='badge-cmp app__flex'>
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className='p-text'>Hello, I am</p>
+            <h1 className='head-text'>Ringtho</h1>
+          </div>
+        </div>
+
+        <div className='tag-cmp app__flex'>
+        <p className='p-text'>Web Developer</p>
+        <p className='p-text'>Freelancer</p>
+        </div>
+       </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile-bg' />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className='overlay-circle'
+          src={images.circle}
+          alt='profile_circle'
+        />
+
+      </motion.div>
+
+      <motion.div>
+
+      </motion.div>
+
     </div>
   )
 }
