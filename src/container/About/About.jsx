7aa5859e9f17662f69e273d0effa144657b464e3
Smith Ringtho <sringtho@gmail.com>
2023-06-02 18:33:16 +0300

    Updated header section designs'

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './About.scss'
+// import { motion } from 'framer-motion'
 
 function About () {
   return (
-    <div>
-      About
+    <>
+    <h2 className='head-text'>
+      I Know That
+      <span>Good Design</span>
+      <br />
+      means
+      <span>Good Business</span>
+    </h2>
+    <div className='app__profiles'>
+
     </div>
+    </>
   )
 }
 
