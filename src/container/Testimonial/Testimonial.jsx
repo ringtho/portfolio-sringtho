0282607c102b822ef69fe5639c87e35777764371
Smith Ringtho <sringtho@gmail.com>
2023-06-04 12:18:49 +0300

    Minor changes

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,65 +1,61 @@
 import React, { useState, useEffect } from 'react'
-import './Testimonial.scss'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 import { motion } from 'framer-motion'
+
 import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
+import './Testimonial.scss'
 
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
-
-function Testimonial () {
-  const [brands, setBrands] = useState([])
-  const [testimonials, setTestimonials] = useState([])
+const Testimonial = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
-
-  useEffect(() => {
-    const query = '*[_type == "testimonials"]'
-    const brandsQuery = '*[_type == "brands"]'
-
-    client.fetch(query)
-      .then(data => {
-        setTestimonials(data)
-      })
-
-    client.fetch(brandsQuery)
-      .then(data => {
-        setBrands(data)
-      })
-  }, [])
-
-  const test = testimonials[currentIndex]
+  const [testimonials, setTestimonials] = useState([])
+  const [brands, setBrands] = useState([])
 
   const handleClick = (index) => {
     setCurrentIndex(index)
   }
 
-  console.log(test)
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]'
+    const brandsQuery = '*[_type == "brands"]'
+
+    client.fetch(query).then((data) => {
+      setTestimonials(data)
+    })
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data)
+    })
+  }, [])
 
   return (
     <>
       {testimonials.length && (
         <>
-          <div className='app__testimonial-item app__flex'>
-            <img src={urlFor(test?.imageurl)} alt='testimonial' />
-            <div className='app__testimonial-content'>
-              <p className='p-text'>{test?.feedback}</p>
+          <div className="app__testimonial-item app__flex">
+            <img
+              src={urlFor(testimonials[currentIndex].imageurl)}
+              alt={testimonials[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
               <div>
-                <h4 className='bold-text'>{test?.name}</h4>
-                <h5 className='p-text'>{test?.company}</h5>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
               </div>
             </div>
           </div>
 
-          <div className='app__testimonial-btns app__flex'>
+          <div className="app__testimonial-btns app__flex">
             <div
-              className='app__flex'
+              className="app__flex"
               onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}
             >
               <HiChevronLeft />
             </div>
 
             <div
-              className='app__flex'
-              onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 - 1 : currentIndex + 1)}
+              className="app__flex"
+              onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}
             >
               <HiChevronRight />
             </div>
@@ -67,8 +63,8 @@ function Testimonial () {
         </>
       )}
 
-      <div className='app__testimonial-brands app__flex'>
-        { brands.map((brand) => (
+      <div className="app__testimonial-brands app__flex">
+        {brands?.map((brand) => (
           <motion.div
             whileInView={{ opacity: [0, 1] }}
             transition={{ duration: 0.5, type: 'tween' }}
